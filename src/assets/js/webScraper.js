40efd1279667c4505076db4fadbfdc57f5721840
Emeka Orji <emekapraiseo@gmail.com>
2022-08-05 21:05:24 +0100

    fix: *

--- src/assets/js/webScraper.js
+++ src/assets/js/webScraper.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 async function getResults(job, location, pageNumber) {
 	const { data } = await axios.get(
-		`https://www.flexjobs.com/search?page=${pageNumber}&search=${job}&location=${location}`
+		`${window.location.href}api/search?page=${pageNumber}&search=${job}&location=${location}`
 	);
 	const $ = cheerio.load(data);
 
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
-	// server: {
-	// 	proxy: {
-	// 		'/api': {
-	// 			target: 'https://www.flexjobs.com',
-	// 			changeOrigin: true,
-	// 			rewrite: (path) => path.replace(/^\/api/, ''),
-	// 		},
-	// 	},
-	// },
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://www.flexjobs.com',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			},
+		},
+	},
 });
